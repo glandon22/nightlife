9b82a3edf1973775c9a9a20c23ef4ae5b3ad78c3
unknown <glandonwright@gmail.com>
2018-01-16 08:03:29 -0500

    updates

--- server/index.js
+++ server/index.js
@@ -54,8 +54,54 @@ app.use(passport.initialize());
 app.use(passport.session());
 */
 
+/* GET home page. */
+app.get('/api/:term', function(req, res, next) {
+  var term = req.params.term;
+  tempQuery = term;
+  client.search({
+    term:'bar',
+    location: term
+  }).then(response => {
+    //save businesses return in object
+    var businesses = response.jsonBody.businesses;  
+    var loopLength = businesses.length >= 10 ? 10 : businesses.length;
+    var responseObject = []; 
+      for (var i = 0; i < loopLength; i++) {
+        //save this long object string to a var for code clarity
+        var marker = response.jsonBody.businesses[i];
+        responseObject.push({
+          name: marker.name,
+          image: marker.image_url,
+          category: marker.categories[0].title,
+          rating: marker.rating,
+          price: marker.price,
+          yelpUrl: marker.url,
+          votes: 0
+        });
+      }
+      mongo.connect(url, function(err,db) {
+        var collection= db.collection('test');
+        var fuck = collection.find({}, {name: 1, votes: 1}).toArray(function(err,data) {
+          for (var i = 0; i < responseObject.length; i++) {
+            for (var j = 0; j < data.length; j++) {
+              if (data[j].name === responseObject[i].yelpUrl) {
+               responseObject[i].votes = data[j].votes; 
+              }
+            }
+          }
+          res.send(responseObject);
+        });
+
+             
+      });
+      
+  }).catch(e => {
+      console.log(e);
+  });
+});
+
 // Answer API requests.
-app.get('/api', function (req, res) {
+app.get('/poopoo', function (req, res) {
   res.set('Content-Type', 'application/json');
   res.send('{"message":"Hello from the custom server!"}');
 });

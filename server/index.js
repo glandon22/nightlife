723a8d6eba81b5b1baaf767a22be813a90e56dab
unknown <glandonwright@gmail.com>
2018-01-16 07:58:17 -0500

    updates

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const path = require('path');
 var passport = require('passport');
 var Strategy = require('passport-twitter').Strategy;
+const yelp = require('yelp-fusion');
+const apiKey = '4OXAW5GC7aWnQiy_pm6K_AwGNP2dTis_CDk48CiKd60uQAp3oiNOU_J_rKWCevn3RGeDS9fsbhX0UHeCpoJbtSc9v9LytN9EuZBejaG0plho5CYExoW-HjgD_dAyWnYx';
+const client = yelp.client(apiKey);
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb://glandon22:taylord22@ds163796.mlab.com:63796/nightlife'
+var user = {};
+var tempQuery = '';
+var redirect = false;
 
 const app = express();
 const PORT = process.env.PORT || 5000;
